edd6f47834087365529160510e27f882877377d4
Dmitry <jussiework@gmail.com>
2023-03-20 20:11:55 +0300

    Hello REact

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Hade from './Hade';
+
+function App() {
+  return (
+    <div className="App">
+      <Hade/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Hade.js
@@ -0,0 +1,12 @@
+import './Hade.css';
+
+function Hade(){
+    return(
+        <>
+            <h1>Hello</h1>
+            <h2>World</h2>
+        </>
+    )
+}
+
+export default Hade;
